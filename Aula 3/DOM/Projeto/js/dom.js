e7c8fb6bfa45cb58ad7d41406db7cfb7ffb4733d
Isac Petinate <isacd81@gmail.com>
2018-08-27 02:47:55 -0300

    Aula 3

---
+++ Aula 3/DOM/Projeto/js/dom.js
@@ -0,0 +1,23 @@
+// Acesssando um Elemento por ID 
+
+console.log(
+    document.getElementById("bandaFavorita")
+)
+
+// Acessando o Elemento pela Classe
+
+console.log(
+    document.getElementsByClassName("carrosFavoritos")[1]
+)
+
+// Acessando o Elemento pela Tag
+
+console.log(
+    document.getElementsByTagName("button")
+)
+
+// Acessando Elemento através do Query
+
+console.log(
+    document.querySelector(".meuHeroiFavorito")
+)
